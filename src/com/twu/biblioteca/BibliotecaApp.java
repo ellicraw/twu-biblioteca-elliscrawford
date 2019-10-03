8dc16f8a04c1898c1f056ebaeaf7348f9847a76b
Ellis Crawford <ellicraw@gmail.com>
2019-10-03 14:52:38 -0400

    getting started

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,6 +1,6 @@
 package com.twu.biblioteca;
 
-public class Bibliotecapp {
+public class BibliotecaApp {
 
     public static void main(String[] args) {
         System.out.println("Hello, world!");
